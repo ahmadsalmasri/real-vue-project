e86d62e50d12e45cb169ad923e310f512f6d1c01
Your Name <ahmad.s.almasri@hotmail.com>
2020-10-08 22:23:30 +0300

    new

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -2,6 +2,10 @@
   <div class="help">
     <h2>Hello From Help Page</h2>
     <h4>Vue Js Greate. It's Easy to Learn</h4>
+    More Info. @
+    <a href="mailto:ahmad.s.almasri@hotmail.com">
+      ahmad.s.almasri@hotmail.com
+    </a>
   </div>
 </template>
 
