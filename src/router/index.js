9ef797756894c43d6205bd067d99303afe649d0a
Your Name <ahmad.s.almasri@hotmail.com>
2020-10-09 15:06:18 +0300

    new

--- src/router/index.js
+++ src/router/index.js
@@ -8,37 +8,37 @@ import Money from "../views/Money.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //component: () =>
-    //import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/help",
-    name: "Help",
-    component: Help
-  },
-  {
-    path: "/money",
-    name: "Money",
-    component: Money
-  }
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        //component: () =>
+        //import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+        path: "/help",
+        name: "Help",
+        component: Help
+    },
+    {
+        path: "/money",
+        name: "Money",
+        component: Money
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes // routes: routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes // routes: routes
 });
 
 export default router;
---
+++ src/views/Money.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="money">
+    <h2>This is an Money Page</h2><hr />
+    <div class="form-group row">
+       <label class="col-2">Hours</label>
+       <input type="text" v-model="hours" />
+    </div>
+    <div class="form-group row">
+       <label class="col-2">Minutes</label>
+       <input type="text" v-model="minutes" />
+    </div>
+    <div class="form-group row">
+       <label class="col-2">Seconds</label>
+       <input type="text" v-model="seconds" />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Money"
+};
+</script> 
