449836c4359794f1b8a1a784db37e9a90c0cb692
Your Name <ahmad.s.almasri@hotmail.com>
2020-10-10 02:15:14 +0300

    first commit

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,34 +1,49 @@
 <template>
   <div class="money">
-    <h2>{{ pageDescreption }}</h2><hr />
+    <h2>{{ pageDescreption }}</h2>
+    <hr />
 
     <div class="alert alert-primary" role="alert">
       This is a primary alert—check it out!
     </div>
     <div class="form-group row">
-       <label class="col-2">Hours</label>
-       <input type="text" v-model="hours" />
+      <label class="col-2">Hours</label>
+      <input type="text" v-model="hours" />
     </div>
     <div class="form-group row">
-       <label class="col-2">Minutes</label>
-       <input type="text" v-model="minutes" />
+      <label class="col-2">Minutes</label>
+      <input type="text" v-model="minutes" />
     </div>
     <div class="form-group row">
-       <label class="col-2">Seconds</label>
-       <input type="text" v-model="seconds" />
+      <label class="col-2">Seconds</label>
+      <input type="text" v-model="seconds" />
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-        data: function() {
-        return {
-            pageName: "Money",
-            pageDescreption: "This is an Money page",
-        }
+  data: function() {
+    return {
+      pageName: "Money",
+      pageDescreption: "This is an Money page",
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  },
+  watch: {
+    hours: function(v) {
+      this.minutes = v * 60;
     },
+    minutes: function(v) {
+      this.hours = v / 60;
+      this.seconds = v * 60;
+    },
+    seconds: function(v) {
+      this.minutes = v / 60;
+    }
+  },
   name: "Money"
 };
 </script> 
