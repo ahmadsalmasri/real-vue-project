e665291c8d6cef26670e95e23aa60895bd243954
Your Name <ahmad.s.almasri@hotmail.com>
2020-10-11 15:41:13 +0300

    first commit

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="header">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link> |
-			<router-link to="/help">Help</router-link> |
-			<router-link to="/money">Money</router-link> |
-            <router-link to="/blog">Blog</router-link>
-		</div>
-	</div>
+  <div class="header">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/help">Help</router-link> |
+      <router-link to="/money">Money</router-link> |
+      <router-link to="/blog">Blog</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "AppNavbar",
+  name: "AppNavbar"
 };
 </script>
