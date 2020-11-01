54e8895c1459fe1013f05ad5760ef769fcfe7fa3
Your Name <ahmad.s.almasri@hotmail.com>
2020-11-01 11:22:02 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-    <div id="app" class="container">
-        <app-navbar />
-        <router-view />
-    </div>
+  <div id="app" class="container">
+    <app-navbarr />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import AppNavbar from "@/components/AppNavbar.vue";
+import AppNavbarr from "@/components/AppNavbar.vue";
 
 export default {
-    name: "App",
-    components: {
-        AppNavbar
-    }
+  name: "App",
+  components: {
+    AppNavbarr
+  }
 };
 </script>
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 
-    a {
-        font-weight: normal;
-        color: #c4b4db;
-        text-decoration: none;
+  a {
+    font-weight: normal;
+    color: #c4b4db;
+    text-decoration: none;
 
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 </style>
